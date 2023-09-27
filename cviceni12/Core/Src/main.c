c9bd50352bf607a5e12aba9c430bc503774666a0
Šimon Špánik <simon.spanik@gmail.com>
2023-09-27 15:42:59 +0200

    commited

--- cviceni12/Core/Src/main.c
+++ cviceni12/Core/Src/main.c
@@ -97,7 +97,10 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  LL_GPIO_SetOutputPin(LD2_GPIO_Port, LD2_Pin);
+	  LL_mDelay(200);
+	  LL_GPIO_ResetOutputPin(LD2_GPIO_Port, LD2_Pin);
+	  LL_mDelay(200);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */

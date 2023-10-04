cc295ca85b5226a76403e2f7af1431e3340705dc
Šimon Špánik <simon.spanik@gmail.com>
2023-10-04 16:42:20 +0200

    c

--- cviceni2/Core/Src/main.c
+++ cviceni2/Core/Src/main.c
@@ -32,6 +32,8 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
  #define LED_TIME_BLINK 300
+ #define LED_TIME_SHORT 100
+ #define LED_TIME_LONG 1000
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -57,14 +59,42 @@ static void MX_USART2_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 volatile uint32_t Tick;
+static uint32_t old_s2;
+static uint32_t old_s1;
+static uint32_t off_time;
+
+
 void blikac(void)
 {
- static uint32_t delay;
+	 static uint32_t delay;
 
- if (Tick > delay + LED_TIME_BLINK) {
- LL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
- delay = Tick;
- }
+	 if (Tick > delay + LED_TIME_BLINK) {
+		 LL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+		 delay = Tick;
+	 }
+}
+
+void tlacitka(void)
+{
+
+	 uint32_t new_s2 = LL_GPIO_IsInputPinSet(S2_GPIO_Port, S2_Pin);
+	 uint32_t new_s1 = LL_GPIO_IsInputPinSet(S1_GPIO_Port, S1_Pin);
+
+	 if (old_s2 && !new_s2) { // falling edge
+		 off_time = Tick + LED_TIME_SHORT;
+		 LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	 }
+	 old_s2 = new_s2;
+
+	 if (old_s1 && !new_s1) { // falling edge
+	 	 off_time = Tick + LED_TIME_LONG;
+	 	 LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	 }
+	 old_s1 = new_s1;
+
+	 if (Tick > off_time) {
+		 LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	 }
 }
 /* USER CODE END 0 */
 
@@ -109,6 +139,7 @@ int main(void)
   {
     /* USER CODE END WHILE */
 	  blikac();
+	  tlacitka();
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -244,19 +275,10 @@ static void MX_GPIO_Init(void)
   LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTC, LL_SYSCFG_EXTI_LINE13);
 
   /**/
-  LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTC, LL_SYSCFG_EXTI_LINE0);
-
-  /**/
   LL_GPIO_SetPinPull(B1_GPIO_Port, B1_Pin, LL_GPIO_PULL_NO);
 
   /**/
-  LL_GPIO_SetPinPull(S2_GPIO_Port, S2_Pin, LL_GPIO_PULL_UP);
-
-  /**/
   LL_GPIO_SetPinMode(B1_GPIO_Port, B1_Pin, LL_GPIO_MODE_INPUT);
-
-  /**/
-  LL_GPIO_SetPinMode(S2_GPIO_Port, S2_Pin, LL_GPIO_MODE_INPUT);
 
   /**/
   EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_13;
@@ -266,11 +288,10 @@ static void MX_GPIO_Init(void)
   LL_EXTI_Init(&EXTI_InitStruct);
 
   /**/
-  EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_0;
-  EXTI_InitStruct.LineCommand = ENABLE;
-  EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
-  EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING;
-  LL_EXTI_Init(&EXTI_InitStruct);
+  GPIO_InitStruct.Pin = S2_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(S2_GPIO_Port, &GPIO_InitStruct);
 
   /**/
   GPIO_InitStruct.Pin = S1_Pin;
@@ -301,10 +322,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
   GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
   LL_GPIO_Init(LED2_GPIO_Port, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  NVIC_SetPriority(EXTI0_1_IRQn, 0);
-  NVIC_EnableIRQ(EXTI0_1_IRQn);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
